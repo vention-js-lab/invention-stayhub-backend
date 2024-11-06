cb614b7cc4ec18aa64f0d3f495cf560c0ec1ca71
Aziza Azizova <aziza.azizova01@gmail.com>
2024-11-06 15:44:09 +0500

    feat: project setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { DatabaseModule } from './modules/database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+    DatabaseModule,
+  ],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
