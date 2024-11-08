875a311c36434e7bdb623935197c603f1f97825e
SurajSulten <suraj.sulten@gmail.com>
2024-11-08 19:05:00 +0500

    fix: add swagger decorators

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,42 +1,18 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @ApiOperation({ summary: 'Register a new user' })
+  @ApiResponse({ status: 201, description: 'User registered successfully' })
+  @ApiResponse({ status: 409, description: 'User with email already exists' })
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
   }
 }
--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,19 +1,48 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'The unique email address of the user',
+    required: true,
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'strongPassword123',
+    description: 'The user password, with a length between 6 and 22 characters',
+    minLength: 6,
+    maxLength: 22,
+    required: true,
+  })
   @IsString()
   @MinLength(6)
   @MaxLength(22)
   password: string;
 
+  @ApiProperty({
+    example: 'John',
+    description:
+      'The first name of the user, with a length between 3 and 20 characters',
+    minLength: 3,
+    maxLength: 20,
+    required: true,
+  })
   @IsString()
   @MinLength(3)
   @MaxLength(20)
   firstName: string;
 
+  @ApiProperty({
+    example: 'Doe',
+    description:
+      'The last name of the user, with a length between 3 and 20 characters',
+    minLength: 3,
+    maxLength: 20,
+    required: true,
+  })
   @IsString()
   @MinLength(3)
   @MaxLength(20)
