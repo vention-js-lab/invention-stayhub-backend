89e3f639c04df2348c771bdd4ec477997fa7e8be
Shahobiddin Ismoilov <shahobiddinismoilov@gmail.com>
2024-11-10 01:07:18 +0500

    fix: delete unnecessary files, add ApiProperty to dto

--- src/modules/auth/dto/refresh-token.dto.ts
+++ src/modules/auth/dto/refresh-token.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class RefreshTokenDto {
+  @ApiProperty({
+    description: 'Refresh token containing user ID',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   refreshToken: string;
