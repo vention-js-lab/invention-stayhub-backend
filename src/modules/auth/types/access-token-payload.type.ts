ee02b936e8335e9b70d40bca37ef78000b18f16b
Shahobiddin Ismoilov <shahobiddinismoilov@gmail.com>
2024-11-10 01:07:18 +0500

    fix: delete unnecessary files, add ApiProperty to dto

--- src/modules/auth/types/access-token-payload.type.ts
+++
@@ -1,7 +0,0 @@
-import { Roles } from '#/shared/constants/user-roles.constants';
-
-export interface AccessTokenPayload {
-  sub: string;
-  userEmail: string;
-  userRole: Roles;
-}
--- src/modules/auth/types/refresh-token-payload.type.ts
+++
@@ -1,3 +0,0 @@
-export interface RefreshTokenPayload {
-  sub: string;
-}
