4ff18647039823a0b42873ee89eeb000c40782e4
Nizomiddin Khudoyberdiev <43596276+blackseriesC63@users.noreply.github.com>
2024-11-10 17:26:47 +0500

    [INV-31]-Improve accommodation table/accommodation folder fix (#11)
    Co-authored-by: blackseriesC6 <blackseries.n63@gmail.com>

--- src/modules/accommodations/modules/accommodations.module.ts
+++ src/modules/accommodations/modules/accommodations.module.ts
@@ -1,8 +1,8 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { Accommodation } from './entities/accommodations.entity';
-import { AccommodationsController } from './accommodations.controller';
-import { AccommodationsService } from './accommodations.service';
+import { Accommodation } from '../entities/accommodations.entity';
+import { AccommodationsController } from '../controllers/accommodations.controller';
+import { AccommodationsService } from '../services/accommodations.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Accommodation])],
