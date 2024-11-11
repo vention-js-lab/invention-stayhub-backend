0f724d3c5b206b5fac8a70cc99b8a3869067cfb6
SurajSulten <suraj.sulten@gmail.com>
2024-11-11 10:33:13 +0500

    feat: create roles guard

---
+++ src/shared/guards/roles.guard.ts
@@ -0,0 +1,34 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  SetMetadata,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Roles } from '../constants/user-roles.constants';
+
+const ROLES_DECORATOR_KEY = 'roles';
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext) {
+    const allowedRoles = this.reflector.getAllAndOverride<Roles[]>(
+      ROLES_DECORATOR_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!allowedRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    return allowedRoles.includes(user.role);
+  }
+}
+
+export const UserRoles = (...roles: Roles[]) => {
+  return SetMetadata(ROLES_DECORATOR_KEY, roles);
+};
