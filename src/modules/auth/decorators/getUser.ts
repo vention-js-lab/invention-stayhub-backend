2786c3f4a084fac6871f38d75d868684921e1e5c
muhammadjon8 <90293805+muhammadjon8@users.noreply.github.com>
2024-11-11 11:08:36 +0500

    proper error handling for getting request.user

--- src/modules/auth/decorators/getUser.ts
+++ src/modules/auth/decorators/getUser.ts
@@ -1,8 +1,15 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 export const GetUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+    if (!request.user) {
+      throw new UnauthorizedException('User not found');
+    }
     return request.user;
   },
 );
