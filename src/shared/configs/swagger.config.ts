e2b7656e61652651a0d57f3d7267e390138d4748
Shahobiddin Ismoilov <shahobiddinismoilov@gmail.com>
2024-11-15 01:06:52 +0500

    feat: add bearer auth for swagger

--- src/shared/configs/swagger.config.ts
+++ src/shared/configs/swagger.config.ts
@@ -1,8 +1,10 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
 export const SwaggerConfig = new DocumentBuilder()
-  .setTitle('InVention Project')
-  .setDescription('REST API')
+  .setTitle('StayHub API by InVention')
+  .setDescription(
+    'A modern accommodation booking platform API developed by InVention, enabling users to register, authenticate, list properties, book stays, and manage reservations. Built with NestJS and TypeORM.',
+  )
   .setVersion('1.0.0')
-  .addTag('NestJS, Postgres, TypeORM')
+  .addBearerAuth()
   .build();
