080d9a9b09332613f39db9d6246d997164802a17
Aziza Azizova <aziza.azizova01@gmail.com>
2024-11-17 19:58:07 +0500

    fix: added required:false in the ApiProperty decorator

--- src/modules/accommodations/dto/requests/update-accommodation.req.ts
+++ src/modules/accommodations/dto/requests/update-accommodation.req.ts
@@ -14,73 +14,107 @@ import { UpdateAccommodationAddressDto } from './update-accommodation-address.re
 import { Type } from 'class-transformer';
 
 export class UpdateAccommodationDto {
-  @ApiProperty({ description: 'Name of the accommodation' })
+  @ApiProperty({ description: 'Name of the accommodation', required: false })
   @IsOptional()
   @IsString()
   name?: string;
 
-  @ApiProperty({ description: 'Description of the accommodation' })
+  @ApiProperty({
+    description: 'Description of the accommodation',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   description?: string;
 
-  @ApiProperty({ description: 'Cover image URL of the accommodation' })
+  @ApiProperty({
+    description: 'Cover image URL of the accommodation',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   coverImage?: string | undefined;
 
-  @ApiProperty({ description: 'Price per night of the accommodation' })
+  @ApiProperty({
+    description: 'Price per night of the accommodation',
+    required: false,
+  })
   @IsOptional()
   @IsNumber()
   price?: number;
 
-  @ApiProperty({ description: 'Availability status of the accommodation' })
+  @ApiProperty({
+    description: 'Availability status of the accommodation',
+    required: false,
+  })
   @IsOptional()
   @IsBoolean()
   available?: boolean;
 
-  @ApiProperty({ description: 'Date when accommodation becomes available' })
+  @ApiProperty({
+    description: 'Date when accommodation becomes available',
+    required: false,
+  })
   @IsOptional()
   @IsDateString()
   availableFrom?: string;
 
   @ApiProperty({
     description: 'Date when accommodation is no longer available',
+    required: false,
   })
   @IsOptional()
   @IsDateString()
   availableTo?: string;
 
-  @ApiProperty({ description: 'Total square meters of the accommodation' })
+  @ApiProperty({
+    description: 'Total square meters of the accommodation',
+    required: false,
+  })
   @IsOptional()
   @IsNumber()
   @Min(1)
   squareMeters?: number;
 
-  @ApiProperty({ description: 'Number of rooms in the accommodation' })
+  @ApiProperty({
+    description: 'Number of rooms in the accommodation',
+    required: false,
+  })
   @IsOptional()
   @IsNumber()
   @Min(1)
   numberOfRooms?: number;
 
-  @ApiProperty({ description: 'Allowed number of people in the accommodation' })
+  @ApiProperty({
+    description: 'Allowed number of people in the accommodation',
+    required: false,
+  })
   @IsOptional()
   @IsNumber()
   allowedNumberOfPeople?: number;
 
-  @ApiProperty({ description: 'Images related to the accommodation' })
+  @ApiProperty({
+    description: 'Images related to the accommodation',
+    required: false,
+  })
   @IsOptional()
   @ValidateNested({ each: true })
   @Type(() => AccommodationImageDto)
   images?: AccommodationImageDto[];
 
-  @ApiProperty({ description: 'Amenities related to the accommodation' })
+  @ApiProperty({
+    description: 'Amenities related to the accommodation',
+    required: false,
+  })
   @IsOptional()
   @ValidateNested()
   @Type(() => AccommodationAmenityDto)
   amenity?: AccommodationAmenityDto;
 
-  @ApiProperty({ description: 'Address related to the accommodation' })
+  @ApiProperty({
+    description: 'Address related to the accommodation',
+    required: false,
+  })
   @IsOptional()
   @ValidateNested()
   @Type(() => UpdateAccommodationAddressDto)
