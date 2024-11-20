ec5911dcffde4c2a43e76953d77a3627666f6476
SurajSulten <suraj.sulten@gmail.com>
2024-11-20 18:27:05 +0500

    fix: remove unnecassary check

--- src/shared/utils/pagination.util.ts
+++ src/shared/utils/pagination.util.ts
@@ -1,10 +1,9 @@
-import { PaginationMetadata } from '../constants/pagination.constant';
 import { PaginationValues } from '../types/pagination.type';
 
 export function getPaginationOffset(page: number, limit: number) {
   return {
-    take: limit ?? PaginationMetadata.Limit,
-    skip: page && page > 0 ? (page - 1) * limit : 0,
+    take: limit,
+    skip: (page - 1) * limit,
   };
 }
 
