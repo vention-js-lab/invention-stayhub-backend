7b3c75a810c61c0e722b08fdbd30cb033643e1ef
Savlatxon Eshonov <134992658+Savesh1508@users.noreply.github.com>
2024-11-27 20:35:47 +0500

    [INV-93] Add/Remove accommodation to/from wishlist (#40)

--- src/modules/wishlists/dto/create-wishlist.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsString, IsUUID } from 'class-validator';
-
-export class CreateWishlistDto {
-  @IsString()
-  @IsUUID('4', { message: 'Accommodation ID must be a valid UUID v4' })
-  accommodationId: string;
-}
